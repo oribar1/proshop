fe391c69c13c71e621253edad2c1b14ad9b9a5e5
oribar1 <oribar9243@gmail.com>
2022-12-17 15:03:38 +0200

    add navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {Container} from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
 function App() {
   return (
     <>
-  <h1>welcome</h1>
+    <Header />
+    <main>
+      <Container>
+       <h1>welcome</h1> 
+      </Container>
+    
+    </main>
+    <Footer />
+  
     </>
   );
 }
